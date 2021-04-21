ef0f5c5325520bd4a051631191b7d515c2cd76f1
Adryelli Reiz <adryellireis0santos@gmail.com>
2021-04-20 21:08:22 -0300

    welcome screen / screen navigation

--- src/pages/Welcome.tsx
+++ src/pages/Welcome.tsx
@@ -6,40 +6,53 @@ import {
     SafeAreaView, 
     StyleSheet,
     Dimensions,
+    View,
 } from 'react-native';
 import { Feather } from '@expo/vector-icons';
+import { useNavigation } from '@react-navigation/core';
 
 import colors from '../../styles/colors';
+import fonts from '../../styles/fonts';
 import wateringImg from '../assets/watering.png';
 
+
 export function Welcome(){
+    const navigation = useNavigation();
+
+    function handleStart() {
+        navigation.navigate('UserIdentification')
+    };
+
     return(
         <SafeAreaView style={styles.container} >
-            <Text style={styles.title} >
-                Gerencie {'\n'}
-                suas plantas {'\n'}
-                de forma fácil</Text>
+            <View style={styles.wrapper} >
+                <Text style={styles.title} >
+                    Gerencie {'\n'}
+                    suas plantas de {'\n'}
+                    forma fácil</Text>
 
-            <Image 
-                source={wateringImg} 
-                style={styles.image} 
-                resizeMode='contain'
-            />
-
-            <Text style={styles.subtitle} >
-                Não esqueça mais de regar suas plantas.
-                 Nós cuidamos de lembrar você sempre que precisar.
-            </Text>
-
-            <TouchableOpacity
-                style={styles.button}
-                activeOpacity={0.7}
-            >
-                <Feather 
-                    name="chevron-right" 
-                    style={styles.buttonIcon}
+                <Image 
+                    source={wateringImg} 
+                    style={styles.image} 
+                    resizeMode='contain'
                 />
-            </TouchableOpacity>
+
+                <Text style={styles.subtitle} >
+                    Não esqueça mais de regar suas plantas.
+                    Nós cuidamos de lembrar você sempre que precisar.
+                </Text>
+
+                <TouchableOpacity
+                    style={styles.button}
+                    activeOpacity={0.7}
+                    onPress={handleStart}
+                >
+                    <Feather 
+                        name="chevron-right" 
+                        style={styles.buttonIcon}
+                    />
+                </TouchableOpacity>
+            </View>
         </SafeAreaView>
     )
 }
@@ -47,21 +60,28 @@ export function Welcome(){
 const styles = StyleSheet.create({
     container: {
         flex: 1,
+    },
+    wrapper: {
+        flex: 1,
         alignItems: 'center',
         justifyContent: 'space-around',
+        paddingHorizontal: 20
     },
     title: {
-        fontSize: 32,
+        fontSize: 28,
         fontWeight: 'bold',
         textAlign: 'center',
         color: colors.heading,
         marginTop: 38,
+        fontFamily: fonts.heading,
+        lineHeight: 34,
     },
     subtitle: {
         textAlign: 'center',
         fontSize: 18,
         paddingHorizontal: 20,
-        color: colors.heading
+        color: colors.heading,
+        fontFamily: fonts.text,
     },
     image: {
         height: Dimensions.get('window').height * 0.5,
